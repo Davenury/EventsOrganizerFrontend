77d91170917cc761ad8ae6a9235587f9ac4dd36b
Dawid Dębowski <dawid199960@gmail.com>
2021-05-12 21:14:38 +0200

    io-75 - gird for list view

--- src/components/instructor_searching/InstructorCoursesListView.js
+++ src/components/instructor_searching/InstructorCoursesListView.js
@@ -1,14 +1,17 @@
+import { Grid } from "@material-ui/core"
 import { CourseDetails } from "../course/CourseDetails"
 
 export const InstructorCoursesListView = (props) => {
 
     const prepareEvents = () => {
-        return props.events.map(event => <CourseDetails course={event} />)
+        return props.events.map(event => <Grid item xs={12} sm={6}><CourseDetails course={event} /></Grid>)
     }
 
     return (
         <div>
-            {prepareEvents()}
+            <Grid container>
+                {prepareEvents()}
+            </Grid>
         </div>
     )
 }
